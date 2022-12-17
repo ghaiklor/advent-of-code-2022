8875c30a6cfe69afb8853595a29006964524d6b8
Eugene Obrezkov <ghaiklor@gmail.com>
2022-12-17 13:52:10 +0200

    [solution]: Day 16-1

--- day-16-1/solution.ts
+++ day-16-1/solution.ts
@@ -7,9 +7,13 @@ type Valve = {
   neighbors: Array<Valve["id"]>;
 };
 
-const file = path.resolve(__dirname, "test.txt");
+const file = path.resolve(__dirname, "input.txt");
 const input = fs.readFileSync(file, "utf-8").slice(0, -1).split("\n");
 const valves: Map<Valve["id"], Valve> = new Map();
+const pathFromTo: Map<
+  `${Valve["id"]}->${Valve["id"]}`,
+  Array<Valve["id"]>
+> = new Map();
 
 // Parsing info about valves
 for (const valve of input) {
@@ -26,33 +30,100 @@ for (const valve of input) {
 
 // Finding the optimal path for all valves and make a map for all combinations
 // This will help to get an optimal path in O(1) later
-let start = "AA";
-let queue: Array<Valve["id"]> = Array.from(valves.keys());
-let hops: Record<Valve["id"], Valve["id"]> = {};
-let minutes: Record<Valve["id"], number> = { [start]: 30 };
-let pressure: Record<Valve["id"], number> = { [start]: 0 };
-let weights: Record<Valve["id"], number> = {
-  [start]: valves.get(start)!.rate * minutes[start],
-};
+// However, optimal here means just the shortest in terms of time
+// The weight of the edges here is always 1, because the time between caves is 1
+// This is the classic Dijkstra algorithm, so nothing to comment here
+for (const start of valves.values()) {
+  const queue: Array<Valve["id"]> = Array.from(valves.keys());
+  const hops: Record<Valve["id"], Valve["id"]> = {};
+  const distance: Record<Valve["id"], number> = { [start.id]: 0 };
 
-while (queue.length > 0) {
-  queue.sort((a, b) => (pressure[b] ?? -Infinity) - (pressure[a] ?? -Infinity));
+  while (queue.length > 0) {
+    queue.sort((a, b) => (distance[a] ?? Infinity) - (distance[b] ?? Infinity));
 
-  const source = queue.shift()!;
-  for (const neighbor of valves.get(source)!.neighbors) {
-    if (!queue.includes(neighbor)) continue;
+    const source = queue.shift()!;
+    for (const neighbor of valves.get(source)!.neighbors) {
+      if (!queue.includes(neighbor)) continue;
 
-    const rate = valves.get(neighbor)!.rate;
-    const weight = weights[source] ?? -Infinity;
-    const alt = weight + rate * (minutes[source] - (rate > 0 ? 2 : 1));
+      const weight = distance[source] ?? Infinity;
+      const alt = weight + 1;
 
-    if (alt > (weights[neighbor] ?? -Infinity)) {
-      weights[neighbor] = alt;
-      minutes[neighbor] = minutes[source] - (rate > 0 ? 2 : 1);
-      pressure[neighbor] = pressure[source] + minutes[neighbor] * rate;
-      hops[neighbor] = source;
+      if (alt < (distance[neighbor] ?? Infinity)) {
+        distance[neighbor] = alt;
+        hops[neighbor] = source;
+      }
     }
+  }
+
+  // When we found the path from source to any of other valves
+  // We can build a map where we assign those path as source->target
+  for (const target of valves.keys()) {
+    let path: Array<Valve["id"]> = [];
+    let hop: Valve["id"] = target;
+
+    if (hops[hop] || hop === start.id) {
+      while (hop) {
+        path.unshift(hop);
+        hop = hops[hop];
+      }
+    }
+
+    pathFromTo.set(`${start.id}->${target}`, path);
   }
 }
 
-console.log(pressure);
+// Now, having all the combinations of path we can take from and to
+// Let's start walking...
+function walk(
+  position: Valve["id"],
+  time: number,
+  pressure: number,
+  opened: Set<Valve["id"]>
+) {
+  // What is the pressure rate right now for opened valves?
+  let rate = Array.from(opened.values()).reduce(
+    (rate, v) => rate + valves.get(v)!.rate,
+    0
+  );
+
+  // Maximux possible pressure in this case, including the past ones
+  let max = pressure + rate * (30 - time);
+
+  // We are interested only in valves that has a positive rate
+  // There is no sense in opening a valve which rate is 0
+  let valvesToOpen = Array.from(valves.values()).filter((v) => v.rate > 0);
+
+  // Those useful valves are all candidates to open
+  for (const valve of valvesToOpen) {
+    // Unless they are already opened
+    if (opened.has(valve.id)) continue;
+
+    // Time spent to go to the valve from current position, including its opening
+    // Because path holds the starting point as well
+    // The length here actually means (realDistance + 1)
+    const timeDelta = pathFromTo.get(`${position}->${valve.id}`)!.length;
+
+    // Now, if it turns out that the time needed to go there is longer than we have
+    // Then it makes no sense to going there, we don't have time for that
+    if (time + timeDelta >= 30) continue;
+
+    // However, if we have the time, we open the valve and calculate new pressure
+    const newTotal = pressure + timeDelta * rate;
+    opened.add(valve.id);
+
+    // Assuming that this strategy works best, we continue walking
+    const value = walk(valve.id, time + timeDelta, newTotal, opened);
+
+    // But if turns out that there are possible routes that leads us to greater pressure
+    // We update our max pressure value with it
+    if (max < value) max = value;
+
+    // And closing the valve, because, turns out, opening it didn't really gave us the max pressure
+    opened.delete(valve.id);
+  }
+
+  return max;
+}
+
+const result = walk("AA", 0, 0, new Set());
+console.log(result);
